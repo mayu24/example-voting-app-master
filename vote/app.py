8e9270b97e4897af67c9e815575a484104e3b2ee
Mayur Mahajan <mayuryesh@gmail.com>
2023-03-16 11:19:56 +0100

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -1,51 +1,68 @@
-from flask import Flask, render_template, request, make_response, g
-from redis import Redis
-import os
-import socket
-import random
-import json
-import logging
-
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
-hostname = socket.gethostname()
+from flask import Flask, render_template, request, redirect, url_for, session
+import redis
 
 app = Flask(__name__)
+app.secret_key = 'my_secret_key'  # Change this to a random string
 
-gunicorn_error_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers.extend(gunicorn_error_logger.handlers)
-app.logger.setLevel(logging.INFO)
+# Redis connection settings
+redis_host = '172.30.180.232'  # Replace with your Redis hostname or IP address
+redis_port = 6379
+redis_password = 'Dev0ops@123'  # Replace with your Redis password
 
-def get_redis():
-    if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
-    return g.redis
+# Connect to Redis
+r = redis.Redis(host=redis_host, port=redis_port, password=redis_password, db=0)
 
-@app.route("/", methods=['POST','GET'])
-def hello():
-    voter_id = request.cookies.get('voter_id')
-    if not voter_id:
-        voter_id = hex(random.getrandbits(64))[2:-1]
 
-    vote = None
+@app.route('/')
+def index():
+    # Check if the user is logged in
+    if 'username' in session:
+        # Get the current vote counts
+        yes_count = r.get('yes') or 0
+        no_count = r.get('no') or 0
+        return render_template('index.html', username=session['username'], yes_count=yes_count, no_count=no_count)
+    else:
+        return redirect(url_for('login'))
 
+
+@app.route('/vote', methods=['POST'])
+def vote():
+    # Check if the user is logged in
+    if 'username' in session:
+        # Get the user's vote
+        vote = request.form.get('vote')
+
+        # Increment the vote count in Redis
+        if vote == 'yes':
+            r.incr('yes')
+        elif vote == 'no':
+            r.incr('no')
+
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
-        redis = get_redis()
-        vote = request.form['vote']
-        app.logger.info('Received vote for %s', vote)
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        redis.rpush('votes', data)
+        # Get the username and password from the form
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-    resp = make_response(render_template(
-        'index.html',
-        option_a=option_a,
-        option_b=option_b,
-        hostname=hostname,
-        vote=vote,
-    ))
-    resp.set_cookie('voter_id', voter_id)
-    return resp
+        # Check if the username and password are correct
+        if username == 'my_username' and password == 'my_password':
+            # Store the username in the session
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+    else:
+        return render_template('login.html')
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+@app.route('/logout')
+def logout():
+    # Remove the username from the session
+    session.pop('username', None)
+    return redirect(url_for('login'))
